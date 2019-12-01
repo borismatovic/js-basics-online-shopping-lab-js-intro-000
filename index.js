cdd2cf97675374a6f380b582f61a6316fcd2fac5
confident-history-3187 <boris.matovic@gmail.com>
2019-12-01 14:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber === undefined) return 'Sorry, we don\'t have a creadit card on file for you.'
+  if(cardNumber === undefined) return 'Sorry, we don\'t have a credit card on file for you.'
 }
